6b842b00ba9d5610b3edcf51017923617bdff099
sumit530 <sp43216@gmail.com>
2023-03-16 15:27:36 +0530

    c

--- src/pages/authentication/sign-up.tsx
+++ src/pages/authentication/sign-up.tsx
@@ -1,114 +1,81 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { Form, FormElement } from "@progress/kendo-react-form";
-import { Button } from "@progress/kendo-react-buttons";
-import { Stepper } from "@progress/kendo-react-layout";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { Button, Card, Checkbox, Label, TextInput } from "flowbite-react";
+import type { FC } from "react";
 
-import { AccountDetails } from "./account-details";
-import { PersonalDetails } from "./personal-details";
-import { PaymentDetails } from "./payment-details";
-
-const stepPages = [AccountDetails, PersonalDetails, PaymentDetails];
-
-export const Signup = () => {
-  const [step, setStep] = React.useState(0);
-  const [formState, setFormState] = React.useState({});
-  const [steps, setSteps] = React.useState([
-    { label: "Account Details", isValid: undefined },
-    { label: "Personal Details", isValid: undefined },
-    { label: "Payment Details", isValid: undefined },
-  ]);
-
-  const lastStepIndex = steps.length - 1;
-  const isLastStep = lastStepIndex === step;
-  const isPreviousStepsValid =
-    steps
-      .slice(0, step)
-      .findIndex((currentStep) => currentStep.isValid === false) === -1;
-
-  const onStepSubmit = React.useCallback(
-    (event) => {
-      const { isValid, values } = event;
-
-      const currentSteps = steps.map((currentStep, index) => ({
-        ...currentStep,
-        isValid: index === step ? isValid : currentStep.isValid,
-      }));
-
-      setSteps(currentSteps);
-      setStep(() => Math.min(step + 1, lastStepIndex));
-      setFormState(values);
-
-      if (isLastStep && isPreviousStepsValid && isValid) {
-        alert(JSON.stringify(values));
-      }
-    },
-    [steps, isLastStep, isPreviousStepsValid, step, lastStepIndex]
-  );
-
-  const onPrevClick = React.useCallback(
-    (event) => {
-      event.preventDefault();
-      setStep(() => Math.max(step - 1, 0));
-    },
-    [step, setStep]
-  );
-
+const SignUpPage: FC = function () {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-      }}
-    >
-      <Stepper value={step} items={steps} />
-      <Form
-        initialValues={formState}
-        onSubmitClick={onStepSubmit}
-        render={(formRenderProps) => (
-          <div style={{ alignSelf: "center" }}>
-            <FormElement style={{ width: 480 }}>
-              {stepPages[step]}
-              <span
-                style={{ marginTop: "40px" }}
-                className={"k-form-separator"}
-              />
-              <div
-                style={{
-                  justifyContent: "space-between",
-                  alignContent: "center",
-                }}
-                className={
-                  "k-form-buttons k-button k-button-md k-rounded-md k-button-solid k-button-solid-bases-end"
-                }
-              >
-                <span style={{ alignSelf: "center" }}>
-                  Step {step + 1} of 3
-                </span>
-                <div>
-                  {step !== 0 ? (
-                    <Button
-                      style={{ marginRight: "16px" }}
-                      onClick={onPrevClick}
-                    >
-                      Previous
-                    </Button>
-                  ) : undefined}
-                  <Button
-                    themeColor={"primary"}
-                    disabled={isLastStep ? !isPreviousStepsValid : false}
-                    onClick={formRenderProps.onSubmit}
-                  >
-                    {isLastStep ? "Submit" : "Next"}
-                  </Button>
-                </div>
-              </div>
-            </FormElement>
+    <div className="flex flex-col items-center justify-center px-6 lg:h-screen lg:gap-y-12">
+      <div className="my-6 flex items-center gap-x-1 lg:my-0">
+        <img
+          alt="Flowbite logo"
+          src="https://flowbite.com/docs/images/logo.svg"
+          className="mr-3 h-12"
+        />
+        <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
+          Flowbite
+        </span>
+      </div>
+      <Card
+        horizontal
+        imgSrc="/images/authentication/create-account.jpg"
+        imgAlt=""
+        className="w-full md:max-w-screen-sm [&>img]:hidden md:[&>img]:w-96 md:[&>img]:p-0 md:[&>*]:w-full md:[&>*]:p-16 lg:[&>img]:block"
+      >
+        <h1 className="mb-3 text-2xl font-bold dark:text-white md:text-3xl">
+          Create a Free Account
+        </h1>
+        <form>
+          <div className="mb-4 flex flex-col gap-y-3">
+            <Label htmlFor="email">Your email</Label>
+            <TextInput
+              id="email"
+              name="email"
+              placeholder="name@company.com"
+              type="email"
+            />
           </div>
-        )}
-      />
+          <div className="mb-6 flex flex-col gap-y-3">
+            <Label htmlFor="password">Your password</Label>
+            <TextInput
+              id="password"
+              name="password"
+              placeholder="••••••••"
+              type="password"
+            />
+          </div>
+          <div className="mb-6 flex flex-col gap-y-3">
+            <Label htmlFor="confirmPassword">Confirm password</Label>
+            <TextInput
+              id="confirmPassword"
+              name="confirmPassword"
+              placeholder="••••••••"
+              type="password"
+            />
+          </div>
+          <div className="mb-6 flex items-center gap-x-3">
+            <Checkbox id="acceptTerms" name="acceptTerms" />
+            <Label htmlFor="acceptTerms">
+              I accept the&nbsp;
+              <a href="#" className="text-primary-700 dark:text-primary-200">
+                Terms and Conditions
+              </a>
+            </Label>
+          </div>
+          <div className="mb-7">
+            <Button type="submit" className="w-full lg:w-auto">
+              Create account
+            </Button>
+          </div>
+          <p className="text-sm text-gray-500 dark:text-gray-300">
+            Already have an account?&nbsp;
+            <a href="#" className="text-primary-600 dark:text-primary-200">
+              Login here
+            </a>
+          </p>
+        </form>
+      </Card>
     </div>
   );
 };
-export default Signup
+
+export default SignUpPage;
