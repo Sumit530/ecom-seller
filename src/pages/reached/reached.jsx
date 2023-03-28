4769064cf645d768eb695863a1749d8810fb1573
sumit530 <sp43216@gmail.com>
2023-03-28 17:52:29 +0530

    c

---
+++ src/pages/reached/reached.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import image from "../../assets/images/thank_you.jpg"
+const Reached = () => {
+  return (
+    <>
+    <div className='w-screen h-screen '>
+        <div className='flex flex-col justify-center items-center'>
+            <img src={image} alt="" />
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Reached
